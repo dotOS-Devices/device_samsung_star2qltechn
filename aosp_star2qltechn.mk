c16fd0bcd1f920641d22275684a9e2046eca4389
Nyoutaaa <shhsdsjshs@gmail.com>
2021-04-09 01:48:14 -0300

    star2qltechn: updating dependencies

--- aosp_star2qltechn.mk
+++ aosp_star2qltechn.mk
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
-# Inherit from starqltechn device
-$(call inherit-product, device/samsung/starqltechn/device.mk)
+# Inherit from star2qltechn device
+$(call inherit-product, device/samsung/star2qltechn/device.mk)
 
 # Inherit some common AOSPK stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
